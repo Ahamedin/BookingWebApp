48f2f2fe73a03806474ded7131c63cef2a8ecd25
Ahamedin <iklashriz@gmail.com>
2025-11-15 13:49:20 +0530

    fixed API error

--- frontend/src/store/useBlockStore.js
+++ frontend/src/store/useBlockStore.js
@@ -2,7 +2,10 @@ import { create } from "zustand";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
+const BASE_URL = import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : import.meta.env.VITE_API_URL;
+
 
 export const useBlockStore = create((set,get) => ({
     blocks:[],
